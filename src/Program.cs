53fa01fd1e9df95ac8b627a53c9e3d5869515855
Jojo5263 <joshua@bittenbruenn.de>
2026-01-23 18:57:18 +0100

    First Commit

---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace MeinSpiel;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var mode = new VideoMode(1920, 1080);
+        var window = new RenderWindow(mode, "Mein SFML Spiel auf Arch");
+
+        
+        window.SetFramerateLimit(60);
+
+        
+        window.Closed += (sender, e) => window.Close();
+
+        
+        while (window.IsOpen)
+        {
+           
+            window.DispatchEvents();
+
+            
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+            {
+                window.Close();
+            }
+
+            // B. Zeichnen (Render)
+            window.Clear(Color.Blue); 
+            
+            window.Display();         
+        }
+    }
+}
